71a9547454ce365e7d4a8d91f813903bc37c6e2d
Kostia Ivanov <semenrikirovi4@gmail.com>
2022-05-27 22:48:20 +0300

    fix: change type of paymentSource to be enum (2 possible values) and refactor its usage [2]

--- src/functions/create-payment/schema.ts
+++ src/functions/create-payment/schema.ts
@@ -3,7 +3,7 @@ import { PaymentSource } from '../../services/payment.service';
 export default {
   type: 'object',
   properties: {
-    paymentSource: { enum: PaymentSource },
+    paymentSource: { enum: Object.values(PaymentSource) },
     destination: { type: 'string' },
     currency: { type: 'string' },
     amount: { type: 'string' },
